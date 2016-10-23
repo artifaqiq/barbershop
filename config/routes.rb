9a07409f14ed106643fc8f66d618e86ca66b2a60
Artur Lomako <artifaqiq@gmail.com>
2016-10-23 16:07:55 +0300

    redirect routing errors to root

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'home#index'
+  
+  if Rails.env.production?
+    match '*path' => redirect('/'), via: :get
+  end
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
