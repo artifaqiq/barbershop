8f6340a3ff5f284a24e2e54b2b4422f72244e2f0
Artur Lomako <artifaqiq@gmail.com>
2016-10-23 17:56:02 +0300

    models Service and Price

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
+  resources :prices
+  resources :services
+  resources :services
+  resources :services
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   root 'home#index'
-  
+
+
+
   if Rails.env.production?
     match '*path' => redirect('/'), via: :get
   end
