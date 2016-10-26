1b4f832c1995453cb26e6fa328f128244996a9b0
Artur Lomako <artifaqiq@gmail.com>
2016-10-26 12:58:24 +0300

    fix pg conflict error 'END'

--- app/controllers/time_api_controller.rb
+++ app/controllers/time_api_controller.rb
@@ -15,14 +15,14 @@ class TimeApiController < ApplicationController
     to = @json['to'].to_datetime
 
     if service = @json['service']
-      frees = FreeTime.where("BEGIN >= :from and END <= :to and SERVICE = :service", from: from, to: to, service: service).
+      frees = FreeTime.where("BEGIN >= :from and 'END' <= :to and SERVICE = :service", from: from, to: to, service: service).
           to_a.map { |x| x.as_json(except: [:id, :created_at, :updated_at]) }
-      busys = BusyTime.where("BEGIN >= :from and END <= :to and SERVICE = :service", from: from, to: to, service: service).
+      busys = BusyTime.where("BEGIN >= :from and 'END' <= :to and SERVICE = :service", from: from, to: to, service: service).
           to_a.map { |x| x.as_json(except: [:id, :created_at, :updated_at]) }
     else
-      frees = FreeTime.where("BEGIN >= :from and END <= :to", from: from, to: to).
+      frees = FreeTime.where("BEGIN >= :from and 'END' <= :to", from: from, to: to).
           to_a.map { |x| x.as_json(except: [:id, :created_at, :updated_at]) }
-      busys = BusyTime.where("BEGIN >= :from and END <= :to", from: from, to: to).
+      busys = BusyTime.where("BEGIN >= :from and 'END' <= :to", from: from, to: to).
           to_a.map { |x| x.as_json(except: [:id, :created_at, :updated_at]) }
 
     end
