7bf432cb35eb80cef41ba0d710d10a850d62ace7
Artur Lomako <artifaqiq@gmail.com>
2016-11-25 16:45:22 +0300

    fix bug (sale validates on service_id)

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -2,5 +2,4 @@ class Sale < ActiveRecord::Base
   has_attachment  :photo,  accept: [:jpg, :png, :gif]
 
   validates :percent, exclusion: { in: [0..100] }
-  validates :service_id, presence: true
 end
